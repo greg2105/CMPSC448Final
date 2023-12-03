ae89f5d71a8e60e9da6c32f0efd999d6b4ed3d89
Greg Dorazio <kinggreg2105@gmail.com>
2023-12-02 23:32:01 -0500

    Update ViT.py

--- ViT.py
+++ ViT.py
@@ -4,16 +4,14 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input
 from vit_keras import vit
 
-# Load CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
 def my_vit_l32(image_size, **kwargs):
     if isinstance(image_size, tuple):
-        image_size = max(image_size)  # Take the maximum dimension for the square image
+        image_size = max(image_size)
     return vit.vit_l32(image_size=image_size, **kwargs)
 
-# Build the ViT model
 model_vit = Sequential([
     Input(shape=(32, 32, 3)),
     my_vit_l32(
@@ -30,9 +28,7 @@ model_vit.compile(optimizer='adam',
 
 model_vit.summary()
 
-# Train the ViT model
 model_vit.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
 
-# Evaluate the ViT model
 test_loss_vit, test_acc_vit = model_vit.evaluate(x_test, y_test)
 print(f'ViT Test accuracy: {test_acc_vit * 100:.2f}%')
